0013dbd1391dd87c23939ba49a296709264c5fd1
tonico2011 <huyco2011@gmail.com>
2019-06-11 20:03:27 +1000

    auth.js change (recheck if it causes problems)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -46,14 +46,14 @@ router.post(
       // See if user not found
       if (!user) {
         //400 bad request
-        res.status(400).json({ erros: [{ msg: "Invalid Credentials" }] });
+        res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
         //400 bad request
-        res.status(400).json({ erros: [{ msg: "Invalid Credentials" }] });
+        res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
       }
 
       // Return jsonwebtoken
